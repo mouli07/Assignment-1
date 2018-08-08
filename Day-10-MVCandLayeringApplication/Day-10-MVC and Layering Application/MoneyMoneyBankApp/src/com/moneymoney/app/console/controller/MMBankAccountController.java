20338e44142f6aa597ead4d87c70e4e599db36c9
Das <moulima.das@capgemini.com>
2018-08-08 18:19:16 +0530

    added new

--- Day-10-MVCandLayeringApplication/Day-10-MVC and Layering Application/MoneyMoneyBankApp/src/com/moneymoney/app/console/controller/MMBankAccountController.java
+++ Day-10-MVCandLayeringApplication/Day-10-MVC and Layering Application/MoneyMoneyBankApp/src/com/moneymoney/app/console/controller/MMBankAccountController.java
@@ -9,9 +9,19 @@ import com.moneymoney.framework.controller.BankController;
 
 public class MMBankAccountController extends BankController {
 
+	BankAccountCollection collection;
+	MMBankFactory MMFact = new MMBankFactory();
+	
+	public void setCollection(BankAccountCollection collection)
+	{
+		this.collection=collection;
+	}
+	
 	@Override
 	public void createNewCurrentAccount(Map<String, Object> arg0) {
 		// TODO Auto-generated method stub
+		
+		collection.addBankAccount(MMFact.createNewCurrentAccount(agr0));
 		
 	}
 
@@ -19,18 +29,35 @@ public class MMBankAccountController extends BankController {
 	public void createNewSavingsAccount(Map<String, Object> arg0) {
 		// TODO Auto-generated method stub
 		
+		collection.addBankAccount(MMFact.createNewSavingsAccount(arg0));
 	}
 
 	@Override
 	public Collection<BankAccount> getAllAccounts() {
 		// TODO Auto-generated method stub
-		return null;
+		return collection.viewAll;
 	}
 
 	@Override
 	public Collection<Customer> getAllCustomers() {
 		// TODO Auto-generated method stub
+		List<Customer> listOfCustomer = new ArrayList<>();
+		(collection.viewAll()).stream().forEach((element)->listOfCustomer.add(element.getAccountHolder()));
+		return listOfCustomer;
+	}
+	
+	public BankAccount getAccountById(int id)
+	{
+		for(BankAccount account:collection.viewAll())
+		{
+		if(account.gerAccountNumber()==id)
+			return account;
+		}
 		return null;
 	}
 
+	public void withdraw(int accNo,double amount)
+	{
+	BankAccount account=getAccountById(accNo);
+	}
 }
